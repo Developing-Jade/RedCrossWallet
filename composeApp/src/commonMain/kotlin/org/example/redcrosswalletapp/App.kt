631ff55a6d1bd0fbe3ea8ab0900c9530250559af
Joachimle <joel0002@stud.kea.dk>
2025-10-15 15:27:25 +0200

    Usable button navigating to next page

--- composeApp/src/commonMain/kotlin/org/example/redcrosswalletapp/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/redcrosswalletapp/App.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import org.example.redcrosswalletapp.ui.HomeScreen
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,28 +24,28 @@ import redcrosswalletapp.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
+    //------------ APP-LEVEL STATE ----------------------------------
+    var showingHome by remember { mutableStateOf(true) }
+
+    //------------ THEME --------------------------------------------
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        Surface(modifier = Modifier.fillMaxSize()) {
+            if (showingHome) {
+                HomeScreen (onStartClicked = { showingHome = false })
+            } else {
+                // Placeholder
+                MainScreen()
             }
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    Text(
+        text = "Kotlin Multiplatform",
+        style = MaterialTheme.typography.headlineMedium,
+        modifier = Modifier
+            .fillMaxSize()
+    )
 }
